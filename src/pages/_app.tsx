90df7a0389e1f93048e3fd35f29230071bb97cb7
Kleverson Kenji <kleversonkenji@gmail.com>
2022-03-22 17:23:01 -0300

    adding _document.tsx file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+// AppProps -> propriedades que o componente pode receber
 import { AppProps } from "next/app";
 
-// AppProps -> propriedades que o componente pode receber
+// App é carregado completamente toda vez que usuário troca de tela
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
