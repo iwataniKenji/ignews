d663b490bf93d0cb1f65d2acd965562f8d60a501
Kleverson Kenji <kleversonkenji@gmail.com>
2022-05-24 21:44:26 -0300

    testing getStaticProps

--- src/tests/Home.spec.tsx
+++ src/tests/Home.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../pages";
+import { stripe } from "../services/stripe";
+import Home, { getStaticProps } from "../pages";
 
 jest.mock("next/router");
 jest.mock("next-auth/react", () => {
@@ -7,11 +8,35 @@ jest.mock("next-auth/react", () => {
     useSession: () => [null, false],
   };
 });
+jest.mock("../services/stripe");
 
 describe("Home page", () => {
   it("renders correctly", () => {
     render(<Home product={{ priceId: "fake-price-id", amount: "R$10,00" }} />);
 
     expect(screen.getByText("for R$10,00 month")).toBeInTheDocument();
+  });
+
+  it("loads initial data", async () => {
+    const retrieveStripePricesMocked = jest.mocked(stripe.prices.retrieve);
+
+    retrieveStripePricesMocked.mockResolvedValueOnce({
+      id: "fake-price-id",
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      // objectContaining -> teste passa se conter informação abaixo
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: "fake-price-id",
+            amount: "$10.00",
+          },
+        },
+      })
+    );
   });
 });
